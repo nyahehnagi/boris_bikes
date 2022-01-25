b5dddf1f08a20b9183827a36d6e701044bf50763
Ed <ed321@hotmail.co.uk>
2022-01-25 17:52:33 +0000

    added release_bike test and method

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,11 @@
 require_relative "../lib/docking_station.rb"
 
 describe DockingStation do 
-
+  describe "#release_bike" do
+    # it "should release bike" do
+    #   docking_station = DockingStation.new
+    #   expect(docking_station).to respond_to(:release_bike)
+    # end
+    it { should respond_to(:release_bike)}
+  end
 end 
