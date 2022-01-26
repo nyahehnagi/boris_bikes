751e76d1a576643d87aabeb1698c202daaeb36c4
Bromley <52469112+nyahehnagi@users.noreply.github.com>
2022-01-26 18:03:53 +0000

    refactored tests to be slightly shorter

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -10,13 +10,8 @@ describe DockingStation do
 
     # we want to write a test that gets a bike and expects the bike to be working
     it "releases a bike that is working" do
-      docking_station = DockingStation.new
-      bike = docking_station.release_bike
-      expect(bike.working?).to eq true
-
-      #bike = subject.release_bike
-      #expect(bike).to be_working
-
+      bike = subject.release_bike
+      expect(bike).to be_working
     end
   end
 
@@ -24,15 +19,13 @@ describe DockingStation do
     it { should respond_to(:dock_bike)}
 
     it "should accept a bike" do
-      docking_station = DockingStation.new
-      expect(docking_station).to respond_to(:dock_bike).with(1).argument
+      expect(subject).to respond_to(:dock_bike).with(1).argument
     end 
 
     it "should dock a bike" do
-      docking_station = DockingStation.new
       bike = Bike.new
-      docking_station.dock_bike(bike)
-      expect(docking_station.bikes).not_to be_empty
+      subject.dock_bike(bike)
+      expect(subject.bikes).not_to be_empty
     end 
   end 
   
@@ -40,15 +33,13 @@ describe DockingStation do
     it { should respond_to(:see_bike)}
 
     it "should return true if there are bikes in the docking station" do
-      docking_station = DockingStation.new
       bike = Bike.new
-      docking_station.dock_bike(bike)
-      expect(docking_station.see_bike).to eq true
+      subject.dock_bike(bike)
+      expect(subject.see_bike).to be true
     end
     
     it "should return false if there are no bikes in the docking station" do
-      docking_station = DockingStation.new
-      expect(docking_station.see_bike).to eq false
+      expect(subject.see_bike).to be false
     end 
   end 
 end
